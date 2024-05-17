b1b66a1b94707d9fbfe8bbcb97830a13fdb11359
Aurorn <aurornispro@gmail.com>
2024-05-17 14:55:04 -0400

    Added columns either side for the menu items

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,19 +1,50 @@
-export default function loadMenu() {
+import foodImagePath1 from '../assets/images/Eggs woodhouse.png';
+import foodImagePath2 from '../assets/images/Eggs woodhouse.png';
+import foodImagePath3 from '../assets/images/Eggs woodhouse.png';
+import drinkImagePath1 from '../assets/images/Glengoolie blue.png';
+import drinkImagePath2 from '../assets/images/Hurricane.png';
+import drinkImagePath3 from '../assets/images/Hurricane.png';
+import woodhouseImagePath from '../assets/images/Woodhouse transparent.png';
+
+function loadMenu() {
   const content = document.getElementById('content');
   content.innerHTML = '';
 
-  const container = document.createElement('div');
-  container.classList.add('menu');
+  const menuContainer = document.createElement('div');
+  menuContainer.classList.add('menu-container');
 
-  const headline = document.createElement('h1');
-  headline.textContent = 'Woodhouse Specials';
-  headline.classList.add('headline');
+  const foodColumn = document.createElement('div');
+  foodColumn.classList.add('menu-column');
 
-  const description = document.createElement('p');
-  description.textContent = '';
-  description.classList.add('description');
+  const drinkColumn = document.createElement('div');
+  drinkColumn.classList.add('menu-column');
 
-  container.appendChild(headline);
-  container.appendChild(description);
-  content.appendChild(container);
+  const foodImages = [foodImagePath1, foodImagePath2, foodImagePath3];
+  foodImages.forEach(src => {
+    const img = document.createElement('img');
+    img.src = src;
+    foodColumn.appendChild(img);
+  });
+
+  const drinkImages = [drinkImagePath1, drinkImagePath2, drinkImagePath3];
+  drinkImages.forEach(src => {
+    const img = document.createElement('img');
+    img.src = src;
+    drinkColumn.appendChild(img);
+  });
+
+  const woodhouseContainer = document.createElement('div');
+  woodhouseContainer.classList.add('woodhouse-container');
+
+  const woodhouseImg = document.createElement('img');
+  woodhouseImg.src = woodhouseImagePath;
+  woodhouseImg.classList.add('woodhouse-image');
+  woodhouseContainer.appendChild(woodhouseImg);
+
+  menuContainer.appendChild(foodColumn);
+  menuContainer.appendChild(woodhouseContainer);
+  menuContainer.appendChild(drinkColumn);
+  content.appendChild(menuContainer);
 }
+
+export default loadMenu;
