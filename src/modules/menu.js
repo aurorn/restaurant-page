e6e9dadd7b16a7e8fc7d374cce3e9f162a37e388
Aurorn <aurornispro@gmail.com>
2024-05-17 15:18:41 -0400

    Added a Click event that explains Menu Items

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -6,6 +6,18 @@ import drinkImagePath2 from '../assets/images/Hurricane.png';
 import drinkImagePath3 from '../assets/images/Hurricane.png';
 import woodhouseImagePath from '../assets/images/Woodhouse transparent.png';
 
+function createMenuItem(src, description) {
+  const img = document.createElement('img');
+  img.src = src;
+  img.alt = description;
+  img.addEventListener('click', () => {
+    const speechBubble = document.querySelector('.speech-bubble');
+    speechBubble.innerText = description;
+    speechBubble.style.display = 'block';
+  });
+  return img;
+}
+
 function loadMenu() {
   const content = document.getElementById('content');
   content.innerHTML = '';
@@ -19,17 +31,25 @@ function loadMenu() {
   const drinkColumn = document.createElement('div');
   drinkColumn.classList.add('menu-column');
 
-  const foodImages = [foodImagePath1, foodImagePath2, foodImagePath3];
-  foodImages.forEach(src => {
-    const img = document.createElement('img');
-    img.src = src;
+  const foodItems = [
+    { src: foodImagePath1, description: 'Delicious Eggs prepared by Woodhouse' },
+    { src: foodImagePath2, description: 'Scrambled Eggs with a touch of parsley' },
+    { src: foodImagePath3, description: 'Eggs Benedict with hollandaise sauce' },
+  ];
+
+  foodItems.forEach(item => {
+    const img = createMenuItem(item.src, item.description);
     foodColumn.appendChild(img);
   });
 
-  const drinkImages = [drinkImagePath1, drinkImagePath2, drinkImagePath3];
-  drinkImages.forEach(src => {
-    const img = document.createElement('img');
-    img.src = src;
+  const drinkItems = [
+    { src: drinkImagePath1, description: 'Glengoolie Blue - Aged 25 years' },
+    { src: drinkImagePath2, description: 'Hurricane Cocktail - A tropical delight' },
+    { src: drinkImagePath3, description: 'Hurricane Cocktail - Another one for good measure' },
+  ];
+
+  drinkItems.forEach(item => {
+    const img = createMenuItem(item.src, item.description);
     drinkColumn.appendChild(img);
   });
 
@@ -41,8 +61,13 @@ function loadMenu() {
   woodhouseImg.classList.add('woodhouse-image');
   woodhouseContainer.appendChild(woodhouseImg);
 
+  const speechBubble = document.createElement('div');
+  speechBubble.classList.add('speech-bubble');
+  speechBubble.innerText = "Click a menu item to learn more!";
+  woodhouseContainer.appendChild(speechBubble);
+
   menuContainer.appendChild(foodColumn);
-  menuContainer.appendChild(woodhouseContainer);
+  menuContainer.appendChild(woodhouseContainer); 
   menuContainer.appendChild(drinkColumn);
   content.appendChild(menuContainer);
 }
