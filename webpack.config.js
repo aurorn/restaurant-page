f5a9dc290ff8f01d21df66cd91afcfa371a07b23
Aurorn <aurornispro@gmail.com>
2024-05-17 09:49:39 -0400

    updated formatting

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  mode: 'development',
   module: {
     rules: [
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        title: 'Restaurant Page',
-        filename: 'index.html',
-        inject: 'body',
-    })
-  ]
+      template: './src/index.html',
+    }),
+  ],
 };
